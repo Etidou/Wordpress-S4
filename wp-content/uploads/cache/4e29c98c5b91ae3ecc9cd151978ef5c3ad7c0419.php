7470b580e7a0b5740b70e3642519ee461fdc8a8c
Etidou <45783736+Etidou@users.noreply.github.com>
2020-04-20 19:23:45 +0200

    responsive done

--- wp-content/uploads/cache/4e29c98c5b91ae3ecc9cd151978ef5c3ad7c0419.php
+++ wp-content/uploads/cache/4e29c98c5b91ae3ecc9cd151978ef5c3ad7c0419.php
@@ -2,5 +2,8 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Etienne Phrabot Développeur passionné et curieux - Portfolio" />
+  <meta name="keywords" content="Portfolio, MMI, Developpeur, étudiant, alternance, full-stack, Chambery" />
+  <meta name="description" content="Je m’appelle Etienne, j’ai 19 ans et j’étudie à l’IUT de Chambéry dans la formation MMI (Métiers du Multimédia et de l’Internet). Il y a 2 ans, j’ai découvert le développement web et depuis, j’essaie de découvrir tout ces préceptes !">
   <?php wp_head() ?>
 </head>
